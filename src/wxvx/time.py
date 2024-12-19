371b18001f942c63037037db6a087a6ea9d39ba3
Paul Madden <paul.madden@noaa.gov>
2024-12-19 20:17:51 +0000

    Add time.py

---
+++ src/wxvx/time.py
@@ -0,0 +1,48 @@
+"""
+Routines for computing cycles, leadtimes, etc.
+"""
+
+from datetime import datetime, timedelta
+from itertools import product
+from typing import overload
+
+
+def cycles(config: dict) -> list[datetime]:
+    start, stop = [datetime.fromisoformat(config["cycles"][key]) for key in ("start", "stop")]
+    step = delta(config["cycles"]["step"])
+    return _enumerate(start, stop, step)
+    # xs = [start]
+    # while (x := xs[-1]) < stop:
+    #     xs.append(x + step)
+    # return xs
+
+
+def delta(step: str) -> timedelta:
+    keys = ["hours", "minutes", "seconds"]
+    args = dict(zip(keys, map(int, step.split(":"))))
+    return timedelta(**args)
+
+
+def leadtimes(config: dict) -> list[timedelta]:
+    start, stop = [delta(config["leadtimes"][key]) for key in ("start", "stop")]
+    step = delta(config["leadtimes"]["step"])
+    return _enumerate(start, stop, step)
+    # xs = [start]
+    # while (x := xs[-1]) < stop:
+    #     xs.append(x + step)
+    # return xs
+
+
+def validtimes(config: dict) -> list[datetime]:
+    return [cycle + leadtime for cycle, leadtime in product(cycles(config), leadtimes(config))]
+
+
+@overload
+def _enumerate(start: datetime, stop: datetime, step: timedelta) -> list[datetime]: ...
+@overload
+def _enumerate(start: timedelta, stop: timedelta, step: timedelta) -> list[timedelta]: ...
+def _enumerate(start, stop, step):
+    xs = [start]
+    while (x := xs[-1]) < stop:
+        xs.append(x + step)
+    return xs
