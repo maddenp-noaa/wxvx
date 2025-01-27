c36aac5c3d0db6cfa5d1900a40fb2ec2aa4f6616
Paul Madden <paul.madden@noaa.gov>
2025-01-27 09:33:33 +0000

    WIP

--- src/wxvx/workflow.py
+++ src/wxvx/workflow.py
@@ -11,8 +11,6 @@ from wxvx.net import fetch, status
 from wxvx.time import TimeCoords, validtimes
 from wxvx.vars import GFSVar, Var
 
-# Tasks
-
 
 @external
 def exists(path: Path, taskname: str):
@@ -74,50 +72,27 @@ def grib_index_remote(url: str, ts: str):
     yield asset(url, lambda: status(url) == 200)
 
 
-# @task
-# def netcdf_file(forecast: Path, rundir: Path):
-#     taskname = "Forecast netCDF file %s" % path
-#     path = rundir / "forecast.nc"
-#     yield taskname
-#     yield asset(path, path.is_file)
-#     yield exists(path=forecast, taskname="Forecast %s" % forecast)
-#     logging.info("%s: Opening forecast %s", taskname, forecast)
-#     with catch_warnings():
-#         simplefilter("ignore")
-#         ds = xr.open_dataset(forecast)
-#     _set_cf_metadata(ds, taskname)
-#     logging.info("Writing forecast to %s", path)
-#     path.parent.mkdir(parents=True, exist_ok=True)
-#     # ds.to_netcdf(path=path)
-#     path.touch()
-
-
-# @tasks
-# def run_directory(config: dict):
-#     keys = ("baseline", "cycles", "forecast", "leadtimes", "rundir", "variables")
-#     baseline, cycles, forecast, leadtimes, rundir, variables = [config[k] for k in keys]
-#     yield "Run directory %s" % rundir
-#     yield [
-#         grib_messages(
-#             baseline=baseline,
-#             cycles=cycles,
-#             leadtimes=leadtimes,
-#             rundir=Path(rundir),
-#             variables=variables,
-#         ),
-#         netcdf_file(forecast=Path(forecast), rundir=Path(rundir)),
-#     ]
+@task
+def forecast_dataset(path: Path):
+    taskname = "Forecast %s" % path
+    fd = xr.Dataset()
+    yield taskname
+    yield asset(fd, lambda: bool(fd))
+    yield exists(path, taskname)
+    logging.info("%s: Opening forecast %s", taskname, path)
+    with catch_warnings():
+        simplefilter("ignore")
+        fd.update(xr.open_dataset(path))
 
 
 @tasks
 def verify_all(config: dict):
     taskname = "Verification"
-    logging.info("%s: Opening forecast %s", taskname, config["forecast"])
-    with catch_warnings():
-        simplefilter("ignore")
-        ds = xr.open_dataset(config["forecast"])  # PM factor out to task with external req
-    _set_cf_metadata(ds, taskname)
-    variables = _variables(needed=config["variables"])
+    variables = set()
+    for var in config["variables"]:
+        levels = var.get("levels", [None])
+        for level in levels:
+            variables.add(Var(name=var["name"], levtype=var["levtype"], level=level))
     var_validtime_pairs = []
     for validtime in validtimes(cycles=config["cycles"], leadtimes=config["leadtimes"]):
         for var in sorted(list(variables))[:2]:
@@ -146,55 +121,4 @@ def verify_one(
     gm = grib_message(var=var, variables=variables, tcoord=validtime, rundir=rundir, url=url)
     yield "Verification of %s at %s" % (var, validtime)
     yield gm
-
-
-# Helpers
-
-
-def _set_cf_metadata(ds: xr.Dataset, taskname: str) -> None:  # PM drive per req'd var
-    logging.info("%s: Setting CF metadata on dataset", taskname)
-    ds.attrs["Conventions"] = "CF-1.8"
-    ds["latitude_longitude"] = int()
-    ds.latitude_longitude.attrs["grid_mapping_name"] = "latitude_longitude"
-    for var, long_name, standard_name, units in (
-        ["HGT", "Geopotential Height", "geopotential_height", "m"],
-        ["REFC", "Composite Reflectivity", "unknown", "dBZ"],
-        ["SPFH", "Specific Humidity", "specific_humidity", "1"],
-        ["T2M", "Temperature", "surface_temperature", "K"],
-        ["TMP", "Temperature", "air_temperature", "K"],
-        ["UGRD", "U-Component of Wind", "eastward_wind", "m s-1"],
-        ["VGRD", "V-Component of Wind", "northward_wind", "m s-1"],
-        ["VVEL", "Vertical Velocity (Pressure],", "lagrangian_tendency_of_air_pressure", "Pa s-1"],
-    ):
-        updates = {
-            "grid_mapping": "latitude_longitude",
-            "long_name": long_name,
-            "standard_name": standard_name,
-            "units": units,
-        }
-        logging.debug("%s: Setting %s on %s", taskname, updates, var)
-        ds[var].attrs.update(updates)
-    for var, long_name, standard_name, units in (
-        ["latitude", "latitude", "latitude", "degrees_north"],
-        ["level", "pressure level", "air_pressure", "hPa"],
-        ["longitude", "longitude", "longitude", "degrees_east"],
-    ):
-        updates = {"long_name": long_name, "standard_name": standard_name, "units": units}
-        logging.debug("%s: Setting %s on %s", taskname, updates, var)
-        ds[var].attrs.update(updates)
-    for var, long_name, standard_name in (
-        ["lead_time", "Forecast Period", "forecast_period"],
-        ["time", "Forecast Reference Time", "forecast_reference_time"],
-    ):
-        updates = {"long_name": long_name, "standard_name": standard_name}
-        logging.debug("%s: Setting %s on %s", taskname, updates, var)
-        ds[var].attrs.update(updates)
-
-
-def _variables(needed: list[dict]) -> set[Var]:
-    variables = set()
-    for var in needed:
-        levels = var.get("levels", [None])
-        for level in levels:
-            variables.add(Var(name=var["name"], levtype=var["levtype"], level=level))
-    return variables
+    # _set_cf_metadata(ds, taskname)
