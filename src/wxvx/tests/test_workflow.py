ecdcc4d934f8c4c926a3ed8a6dfb8c5d93869906
Paul Madden <paul.madden@noaa.gov>
2025-01-29 22:54:26 +0000

    Unit tests @ 94%

--- src/wxvx/tests/test_workflow.py
+++ src/wxvx/tests/test_workflow.py
@@ -44,35 +44,41 @@ def test_workflow_forecast_var(check_cf_metadata, da, tmp_path, validtime):
     check_cf_metadata(xr.open_dataset(refs(val))["HGT"])
 
 
-def test_workflow_grib_message():
-    pass
+def test_workflow_grib_message(fakefs, idxdata, testvars, url, validtime):
+    var = variables.Var(name="t", levtype="isobaricInhPa", level="900")
+    with patch.object(workflow, "grib_index_data") as grib_index_data:
+        grib_index_data().ready = False
+        val = workflow.grib_message(
+            var=var, variables=testvars, validtime=validtime, rundir=fakefs, url=url
+        )
+        path = refs(val)
+        assert not path.exists()
+        grib_index_data().ready = True
+        grib_index_data()._assets = asset(idxdata, lambda: True)
+        with patch.object(workflow, "fetch") as fetch:
+            fetch.side_effect = lambda taskname, url, path, headers: path.touch()
+            path.parent.mkdir(parents=True, exist_ok=True)
+            workflow.grib_message(
+                var=var, variables=testvars, validtime=validtime, rundir=fakefs, url=url
+            )
+        assert path.exists()
 
 
-def test_workflow_grib_index_data(fakefs, ts, url, validtime):
-    idxdata = """
+def test_workflow_grib_index_data(fakefs, idxdata, testvars, ts, url, validtime):
+    gribidx = """
     1:0:d=2024040103:HGT:900 mb:anl:
     2:1:d=2024040103:FOO:900 mb:anl:
     3:2:d=2024040103:TMP:900 mb:anl:
     """
     idxfile = fakefs / "gfs.idx"
-    idxfile.write_text(dedent(idxdata).strip())
+    idxfile.write_text(dedent(gribidx).strip())
     grib_index_local = Mock()
     grib_index_local()._assets = asset(idxfile, idxfile.exists)
-    vars2idx = {
-        variables.Var(name=name, levtype="isobaricInhPa", level="900") for name in ("gh", "t")
-    }
     with patch.object(workflow, "grib_index_local", grib_index_local):
         val = workflow.grib_index_data(
-            variables=vars2idx, validtime=validtime, rundir=fakefs, url=url, ts=ts
+            variables=testvars, validtime=validtime, rundir=fakefs, url=url, ts=ts
         )
-    assert refs(val) == {
-        "gh-isobaricInhPa-0900": variables.GFSVar(
-            name="HGT", levstr="900 mb", firstbyte=0, lastbyte=0
-        ),
-        "t-isobaricInhPa-0900": variables.GFSVar(
-            name="TMP", levstr="900 mb", firstbyte=2, lastbyte=2
-        ),
-    }
+    assert refs(val) == idxdata
 
 
 def test_workflow_grib_index_local(fakefs, ts, url, validtime):
@@ -111,6 +117,23 @@ def test_workflow_verify_one():
 @fixture
 def fakefs(fs):
     return Path(fs.create_dir("/test").path)
+
+
+@fixture
+def idxdata():
+    return {
+        "gh-isobaricInhPa-0900": variables.GFSVar(
+            name="HGT", levstr="900 mb", firstbyte=0, lastbyte=0
+        ),
+        "t-isobaricInhPa-0900": variables.GFSVar(
+            name="TMP", levstr="900 mb", firstbyte=2, lastbyte=2
+        ),
+    }
+
+
+@fixture
+def testvars():
+    return {variables.Var(name=name, levtype="isobaricInhPa", level="900") for name in ("gh", "t")}
 
 
 @fixture
