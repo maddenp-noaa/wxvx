16dea3c25ebf69d8f8118a201d8acb5f4f0a44fc
Paul Madden <paul.madden@noaa.gov>
2025-04-15 17:52:34 +0000

    Append .tmp to atomic tempfiles, do not use .with_suffix()

--- src/wxvx/util.py
+++ src/wxvx/util.py
@@ -21,7 +21,7 @@ class WXVXError(Exception): ...
 @contextmanager
 def atomic(path: Path) -> Iterator[Path]:
     path.parent.mkdir(parents=True, exist_ok=True)
-    tmp = path.with_suffix(".tmp")
+    tmp = Path("%s.tmp" % path)
     yield tmp
     tmp.rename(path)
 
